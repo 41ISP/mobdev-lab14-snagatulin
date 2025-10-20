c839db42a04e183e93e263c2f355d38fbba73b02
Ura44 <snagatulini@gmail.com>
2025-10-20 06:06:20 +0000

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mobdev-lab14-snagatulin/sw.js', { scope: '/mobdev-lab14-snagatulin/' })})}
